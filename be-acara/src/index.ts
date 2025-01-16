0d0461e9d307b5597409a3dccdd0c51c32ac5e8d
tofanajisatria <tofanajisatria@gmail.com>
2025-01-16 09:18:11 +0700

    Setup Project : Express

--- be-acara/src/index.ts
+++ be-acara/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import route from "./routes/api";
+import router from "./routes/api";
 
 const app = express();
 
 const PORT = 3000;
+app.use("/api", router);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
